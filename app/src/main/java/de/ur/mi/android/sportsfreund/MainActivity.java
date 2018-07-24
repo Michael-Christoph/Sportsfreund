40fb1eb19f915c5cedd0e68cd7e98092bbd378cf
timo <t.haunberger@gmail.com>
2018-07-24 17:09:05 +0200

    added ActionBar with options to select between games nearby and signed-on-games
    added menu recource file: menu.xml
    
    added drawables: ic_my_location_black_24dp.xml / ic_person_black_24dp.xml
    
    prepaired basic structure for list view in MainActivity

--- app/src/main/java/de/ur/mi/android/sportsfreund/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/sportsfreund/MainActivity.java
@@ -1,13 +1,67 @@
 package de.ur.mi.android.sportsfreund;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ActionBar zum umschalten zwischen Teilgenommenen und Spielen in der Nähe
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.games_nearby);
+
     }
+
+    //Instanziieren des Action menüs
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //On selected Methode für MenuiTEMS
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.signedIn:
+                actionBar.setTitle(R.string.games_signd_in);
+                sortByProximity();
+                populateList();
+
+                break;
+            case R.id.nearby:
+                actionBar.setTitle(R.string.games_nearby);
+                getSignedInGames();
+                populateList();
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sortByProximity()  {
+        // Sortieren der Spiele aus Firebase
+    }
+
+    private void getSignedInGames()  {
+        //Auswahl der angemeldeten spiele aus localer Datenbank
+    }
+
+    private void populateList()  {
+        //Erzeugen des Listview
+    }
+
 }
