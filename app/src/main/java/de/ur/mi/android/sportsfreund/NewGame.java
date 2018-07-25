76c8a1a3f03e1b68dd7ac39a95c2eeff8c1c56a8
max-payne <MaxHierl@web.de>
2018-07-26 00:12:47 +0200

    Hab die NewGame-Activity weiter und Game Klasse angefangen

--- app/src/main/java/de/ur/mi/android/sportsfreund/NewGame.java
+++ app/src/main/java/de/ur/mi/android/sportsfreund/NewGame.java
@@ -1,9 +1,14 @@
 package de.ur.mi.android.sportsfreund;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewGame extends AppCompatActivity {
+    private Button mapButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,27 @@ public class NewGame extends AppCompatActivity {
         // back-button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        setupMapButton();
+
 
     }
-}
+
+    private void setupMapButton(){
+        mapButton = (Button)findViewById(R.id.button_find_place);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToMapActivity();
+            }
+        });
+    }
+
+    private void changeToMapActivity() {
+
+        Intent i = new Intent(this, MapsActivity.class);
+
+        startActivity(i);
+    }
+    }
+
