057e7e95935579de94df3cecd25869f57cb706c7
Michael-Christoph <mpilarski@gmx.net>
2018-07-26 18:17:40 +0200

    Firebase Auth hinzugefuegt RegisterActivity hinzugefuegt

--- app/src/main/java/de/ur/mi/android/sportsfreund/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/sportsfreund/MainActivity.java
@@ -1,7 +1,11 @@
 package de.ur.mi.android.sportsfreund;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textview);
+
+
+
+
+
+        Button testRegisterButton = findViewById(R.id.register_test_button);
+        testRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/de/ur/mi/android/sportsfreund/RegisterActivity.java
@@ -0,0 +1,94 @@
+package de.ur.mi.android.sportsfreund;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+//written with advise from https://www.androidhive.info/2016/06/android-getting-started-firebase-simple-login-registration-auth/
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.editText_mail);
+        editTextPassword = (EditText) findViewById(R.id.editText_password);
+        buttonRegister = (Button) findViewById(R.id.register_button);
+        progressBar = findViewById(R.id.register_progressbar);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(),"Bitte trage deine Email-Adresse ein!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(),"Bitte trage ein Passwort ein!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6){
+                    Toast.makeText(getApplicationContext(), "Dein Passwort ist zu kurz, verwende wenigstens" +
+                            " sechs Zeichen!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email,password).
+                        addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(RegisterActivity.this,
+                                        "createUserWithEmail:onComplete:" + task.isSuccessful(),
+                                        Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this,
+                                            "Die Registrierung ist fehlgeschlagen." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+}
